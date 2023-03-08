7d8ccff8973449e09785039e9a3784c3f1af1aef
saulofreitas-mb <120680201+saulofreitas-mb@users.noreply.github.com>
2023-03-08 15:19:08 -0300

    CREATE pytenable.py

---
+++ script/pytenable.py
@@ -0,0 +1,15 @@
+from tenable.io import TenableIO
+
+tio = TenableIO (
+access_key = os.environ['TENABLE_ACCESS_KEY'],
+secret_key = os.environ['TENABLE_SECRET_KEY'])
+
+scans = tio.scans.list()
+for scan in scans:
+    print(f'Scan {scan["id"]} is named {scan["name"]}')
+
+scan = tio.scans.create(
+    name='External Example Scan',
+    targets=['https://brokencrystals.com/']
+)
+tio.scans.launch(scan['id'])    
