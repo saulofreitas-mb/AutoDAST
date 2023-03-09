2611cf043940cd650a304b7e78f87f3f24f0e66e
saulofreitas-mb <120680201+saulofreitas-mb@users.noreply.github.com>
2023-03-09 17:14:27 -0300

    update pytenable.py

--- script/pytenable.py
+++ script/pytenable.py
@@ -1,19 +1,25 @@
-from tenable.io import TenableIO
 import os
+from tenable_io.client import TenableIOClient
+
+client = TenableIOClient(access_key=os.environ['TENABLE_ACCESS_KEY'], secret_key=os.environ['TENABLE_SECRET_KEY'])
+scan = client.scan_helper.create(name='Saulo AUTODAST SCAN}', text_targets='http://brokencrystals.com/', template='basic')
+scan.launch().download('Saulo_AUTODAST_SCAN.pdf', scan.histories()[0].history_id) 
 
 
-tio = TenableIO (
-vendor= 'Saulo Freitas',
-product='scan ',    
-access_key = os.environ['TENABLE_ACCESS_KEY'],
-secret_key = os.environ['TENABLE_SECRET_KEY'])
 
-scans = tio.scans.list()
-for scan in scans:
-    print(f'Scan {scan["id"]} is named {scan["name"]}')
 
-scan = tio.scans.create(
-    name='External Example Scan',
-    targets=['https://brokencrystals.com/']
-)
-tio.scans.launch(scan['id'])    
+# tio = TenableIO (
+# vendor= 'Saulo Freitas',
+# product='scan ',    
+# access_key = os.environ['TENABLE_ACCESS_KEY'],
+# secret_key = os.environ['TENABLE_SECRET_KEY'])
+
+# scans = tio.scans.list()
+# for scan in scans:
+#     print(f'Scan {scan["id"]} is named {scan["name"]}')
+
+# scan = tio.scans.create(
+#     name='External Example Scan',
+#     targets=['https://brokencrystals.com/']
+# )
+# tio.scans.launch(scan['id'])    
