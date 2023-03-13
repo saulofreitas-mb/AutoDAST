e8591dc0badac343b8128a0cd46519ee237d4c2e
saulofreitas-mb <120680201+saulofreitas-mb@users.noreply.github.com>
2023-03-13 15:51:02 -0300

    UPDATE pytenable

--- script/pytenable.py
+++ script/pytenable.py
@@ -2,12 +2,39 @@ import os
 from tenable_io.client import TenableIOClient
 
 client = TenableIOClient(access_key=os.environ['TENABLE_ACCESS_KEY'], secret_key=os.environ['TENABLE_SECRET_KEY'])
-scan = client.scan_helper.create(name='Saulo AUTODAST SCAN}', text_targets='http://brokencrystals.com/', template='basic')
+scan = client.scan_helper.create(name='Saulo AUTODAST SCAN}', text_targets=['http://brokencrystals.com/'], template='basic')
 scan.launch().download('Saulo_AUTODAST_SCAN.pdf', scan.histories()[0].history_id) 
 
 
 
 
+
+
+
+# Internal scans
+
+# from tenable_io.client import TenableIOClient
+# from tenable_io.api.scans import ScanCreateRequest
+# from tenable_io.api.models import ScanSettings
+# client = TenableIOClient(access_key='{YOUR ACCESS KEY}', secret_key='{YOUR SECRET KEY}')
+# scanners = {scanner.name: scanner.id for scanner in client.scanners_api.list().scanners}
+# template = client.scan_helper.template(name='basic')
+# scan_id = client.scans_api.create(
+#  ScanCreateRequest(
+#  template.uuid,
+#  ScanSettings(
+#  ‘{YOUR SCAN NAME}’,
+#  ‘{YOUR SCAN TARGETS}’,
+#  scanner_id=scanners['{YOUR SCANNER NAME}']
+#  )
+#  )
+# )
+# scan = client.scan_helper.id(scan_id)
+# scan.launch()
+
+
+
+#another implementation
 # tio = TenableIO (
 # vendor= 'Saulo Freitas',
 # product='scan ',    
